2f26245afc480b39ca97e30735953be34f35eeea
hhhhmmmmm02 <167817514+hhhhmmmmm02@users.noreply.github.com>
2024-05-18 16:43:39 -0400

    Added the copyright information  and minor changes to code for clarification

--- CTFforPyhop_and_GUI.py
+++ CTFforPyhop_and_GUI.py
@@ -1,9 +1,25 @@
 """
-Created by Hector Munoz-Avila
+Copyright 2024 Hector Munoz-Avila
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
 Includes:
 
 - A self-created implementation for a sinple capture the Flag Game: 3 blue vs 3 red players, 3 flag locations 
-- 
+- A simple GUI
+- Uses Pyhop 1.2.2: https://bitbucket.org/dananau/pyhop/src/master/
+- Simply run and it will import pyhop and necessary lines
+- Will call Pyhop to generate a plan to CTF. Pyhop is controlling Blue
+- Opponent is red, which selects random flag locations
+
+Key variables:
+
+state.drawTheGrid = True  ## currently set on True so it will display the board; if set on False nothing is displayed
+maxTurns = 100            ## currently set to 100 turns
+
+
 
 """
 
@@ -18,7 +34,10 @@ import time
 def CTFtraining(state):
     #### call this function with 
 
-
+    aiPlayer = ['blue',0]
+    locDest = state.flagLocs[random.randrange(0,len(state.flagLocs))]
+    print("execute: Blue moves: ",[0,1,2],state.blueGoals)
+    print("execute: Red moves: ",[0,1,2],state.redGoals)
     for i in range(0,maxTurns):
         action = returnNextAIAction(state,aiPlayer,locDest)
         #print(action)
@@ -728,28 +747,23 @@ state= drawGrid(state)
 
 
 maxTurns = 100
-aiPlayer = ['blue',0]
-locDest = state.flagLocs[random.randrange(0,len(state.flagLocs))]
 
 print("game start: ")
 printFlagLocs(state)
 printFlagsOwner(state)
 printPlayers(state)
-print("execute: Blue moves: ",[0,1,2],state.blueGoals)
-print("execute: Red moves: ",[0,1,2],state.redGoals)
+
 
 
 
 #input("enter")
 
-### call one of the following if trainign (learning) or just planning
-
+### call one of the following if training (learning) or just planning
 ### CTFtraining(state)
-
-
 CTFplanning(state)
 
-input("end")
+if state.drawTheGrid == True:
+    input("end")
 
 
 
