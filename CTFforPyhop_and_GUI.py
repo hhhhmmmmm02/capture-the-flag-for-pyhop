90b3ca983201103a81cd5dc376e5daae8b782546
hhhhmmmmm02 <167817514+hhhhmmmmm02@users.noreply.github.com>
2024-07-22 22:35:32 -0400

    Mark as discontinued CTFforPyhop_and_GUI.py
    Discontinued;

--- CTFforPyhop_and_GUI.py
+++ CTFforPyhop_and_GUI.py
@@ -1,4 +1,6 @@
 """
+DISCONTINUED - USE CTFForPyhop_and_TK.py instead
+
 Copyright 2024 Hector Munoz-Avila
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
